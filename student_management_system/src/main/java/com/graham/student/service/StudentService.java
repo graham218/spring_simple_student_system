2a21a17aea4a45e6ac6d93918646e2b2f73efc27
BILL GRAHAM <grahambill011@gmail.com>
2021-10-07 23:12:15 +0300

    service layer

--- student_management_system/src/main/java/com/graham/student/service/StudentService.java
+++ student_management_system/src/main/java/com/graham/student/service/StudentService.java
@@ -1,5 +1,10 @@
 package com.graham.student.service;
 
+import java.util.List;
+
+import com.graham.student.entity.Student;
+
 public interface StudentService {
+	List<Student> getAllStudents();
 	
 }
--- student_management_system/src/main/java/com/graham/student/service/impl/StudentServiceImpl.java
+++ student_management_system/src/main/java/com/graham/student/service/impl/StudentServiceImpl.java
@@ -1,7 +1,21 @@
 package com.graham.student.service.impl;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.graham.student.entity.Student;
+import com.graham.student.repository.StudentRepository;
 import com.graham.student.service.StudentService;
 
+@Service
 public class StudentServiceImpl implements StudentService {
+
+	private StudentRepository studentRepository;
+	@Override
+	public List<Student> getAllStudents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
