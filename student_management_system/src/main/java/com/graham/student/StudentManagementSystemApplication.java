1d364f8216d840c67bc34b21446d2baf36d084c4
BILL GRAHAM <grahambill011@gmail.com>
2021-10-08 00:31:29 +0300

    Update StudentManagementSystemApplication.java

--- student_management_system/src/main/java/com/graham/student/StudentManagementSystemApplication.java
+++ student_management_system/src/main/java/com/graham/student/StudentManagementSystemApplication.java
@@ -1,13 +1,20 @@
 package com.graham.student;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
 	}
 
 }
