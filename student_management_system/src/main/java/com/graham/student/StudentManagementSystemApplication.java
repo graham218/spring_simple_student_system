f61028e98b6b24f08066d33db3f9ee2a79f68bda
BILL GRAHAM <grahambill011@gmail.com>
2021-10-08 00:39:46 +0300

    Update StudentManagementSystemApplication.java

--- student_management_system/src/main/java/com/graham/student/StudentManagementSystemApplication.java
+++ student_management_system/src/main/java/com/graham/student/StudentManagementSystemApplication.java
@@ -1,8 +1,12 @@
 package com.graham.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.graham.student.entity.Student;
+import com.graham.student.repository.StudentRepository;
 
 @SpringBootApplication
 public class StudentManagementSystemApplication implements CommandLineRunner{
@@ -10,10 +14,20 @@ public class StudentManagementSystemApplication implements CommandLineRunner{
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
+	
+	@Autowired
+	private StudentRepository studentRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		Student student1=new Student("Bill","Graham","grahambill011@gmail.com");
+		studentRepository.save(student1);
 		
+		Student student2=new Student("Charlton","Cole","charltolcole@gmail.com");
+		studentRepository.save(student2);
+		
+		Student student3=new Student("Darren","Bent","darrentbent@gmail.com");
+		studentRepository.save(student3);
 		
 	}
 
