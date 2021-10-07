00f95ee46a1b01dc45c28c1682774556d222c022
BILL GRAHAM <grahambill011@gmail.com>
2021-10-07 23:26:00 +0300

    Update StudentController.java

--- student_management_system/src/main/java/com/graham/student/controller/StudentController.java
+++ student_management_system/src/main/java/com/graham/student/controller/StudentController.java
@@ -1,8 +1,23 @@
 package com.graham.student.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.graham.student.service.StudentService;
 
 @Controller
 public class StudentController {
+	private StudentService studentservice;
+
+	public StudentController(StudentService studentservice) {
+		super();
+		this.studentservice = studentservice;
+	}
 	
+	//handler method to handle list students and return mode and view
+	@GetMapping("/students")
+	public String listStudents(Model model) {
+		model.addAttribute("students", studentservice.getAllStudents());
+	}
 }
