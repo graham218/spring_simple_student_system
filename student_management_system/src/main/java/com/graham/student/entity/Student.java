a7ed1132b50fca4b2f6bbec9ccaffd10f83d20d6
BILL GRAHAM <grahambill011@gmail.com>
2021-10-07 22:33:24 +0300

    Update Student.java

--- student_management_system/src/main/java/com/graham/student/entity/Student.java
+++ student_management_system/src/main/java/com/graham/student/entity/Student.java
@@ -1,11 +1,26 @@
 package com.graham.student.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="students")
+
 public class Student {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String firstname;
 	private String lastName;
 	private String email;
 	
+	public Student() {
+		
+	}
 	
 	//constructor using fields
 	public Student(String firstname, String lastName, String email) {
