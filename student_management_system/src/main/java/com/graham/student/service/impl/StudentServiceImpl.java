e4ed35f029d3a7905915f523167bf17b74f4f8ee
BILL GRAHAM <grahambill011@gmail.com>
2021-10-08 02:51:55 +0300

    Update StudentServiceImpl.java

--- student_management_system/src/main/java/com/graham/student/service/impl/StudentServiceImpl.java
+++ student_management_system/src/main/java/com/graham/student/service/impl/StudentServiceImpl.java
@@ -30,4 +30,16 @@ public class StudentServiceImpl implements StudentService {
 		return studentRepository.save(student);
 	}
 
+
+	@Override
+	public Student getStudentById(Long id) {
+		return studentRepository.findById(id).get();
+	}
+
+
+	@Override
+	public Student updateStudent(Student student) {
+		return studentRepository.save(student);
+	}
+
 }
